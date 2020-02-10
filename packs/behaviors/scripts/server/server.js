6b8c66f84895655c051b43b26c2c1acdc4daba88
dzx-dzx <dzx_dzx@hotmail.com>
2020-02-10 18:47:12 +0800

    Refactor.

--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -3,50 +3,28 @@ var serverSystem = server.registerSystem(0, 0);
 import { Coordinate, Position, BlockType } from '../utils';
 
 serverSystem.initialize = function () {
-    
+
     const scriptLoggerConfig = serverSystem.createEventData("minecraft:script_logger_config");
     scriptLoggerConfig.data.log_errors = true;
     scriptLoggerConfig.data.log_information = true;
     scriptLoggerConfig.data.log_warnings = true;
     serverSystem.broadcastEvent("minecraft:script_logger_config", scriptLoggerConfig);
 
-    serverSystem.registerEventData("NormaConstructor:getPosition",{
-            position: new Position(
-                new Coordinate(undefined, undefined, undefined),
-                undefined
-            ),
-            playerID: undefined
-        })
-    serverSystem.registerEventData("NormaConstructor:getBlockType",{
-            blockType: new BlockType(undefined, undefined),
-            playerID: undefined
-        })
+    serverSystem.registerEventData("NormaConstructor:getPosition", {
+        position: new Position(
+            new Coordinate(undefined, undefined, undefined),
+            undefined
+        ),
+        playerID: undefined
+    })
+    serverSystem.registerEventData("NormaConstructor:getBlockType", {
+        blockType: new BlockType(undefined, undefined),
+        playerID: undefined
+    })
     serverSystem.registerEventData("NormaConstructor:command", { command: undefined, playerID: undefined })
     serverSystem.registerEventData("NormaConstructor:ExecutionRequest", { playerID: undefined })
 
-    serverSystem.listenForEvent("minecraft:player_placed_block", (eventData) => {
-
-        let blockType = new BlockType(undefined, undefined)
-
-        let handContainer = serverSystem.getComponent(eventData.data.player, "minecraft:hand_container").data
-        let offHandItem = handContainer[1]
-        if (offHandItem.__identifier__ == "minecraft:shield") {//Since the player can't place air, holding a shield will represent so. 
-            blockType.blockIdentifier = "minecraft:air"
-            blockType.blockState = null
-        }
-        else {
-            let tickingArea = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.ticking_area
-            let block = serverSystem.getBlock(tickingArea, eventData.data.block_position)
-            blockType.blockIdentifier = block.__identifier__
-            blockType.blockState = serverSystem.getComponent(block, "minecraft:blockstate").data
-        }
-
-        let getblockTypeEventData = serverSystem.createEventData("NormaConstructor:getBlockType")
-        getblockTypeEventData.data.blockType = blockType
-        getblockTypeEventData.data.playerID = eventData.data.player.id
-        serverSystem.broadcastEvent("NormaConstructor:getBlockType", getblockTypeEventData)
-
-    })
+    serverSystem.listenForEvent("minecraft:player_placed_block", (eventData) => getBlockType(eventData))
     //TODO:Consider switching to "minecraft:entity_use_item"
     serverSystem.listenForEvent("minecraft:block_interacted_with", (eventData) => {
         let playerID = eventData.data.player.id
@@ -56,51 +34,27 @@ serverSystem.initialize = function () {
         switch (mainHandItem) {
             case "minecraft:wooden_axe": {
                 //Set position.
-                let position = new Position(
-                    new Coordinate(undefined, undefined, undefined),
-                    undefined
-                )
-
-                position.coordinate = eventData.data.block_position
-                position.tickingArea = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.ticking_area
-
-                let getPositionEventData = serverSystem.createEventData("NormaConstructor:getPosition")
-                getPositionEventData.data.position = position
-                getPositionEventData.data.playerID = playerID
-                serverSystem.broadcastEvent("NormaConstructor:getPosition", getPositionEventData)
-
+                getPosition(eventData)
                 break;
             }
             case "minecraft:stone_axe": {
                 //Remove the last position.
-                let commandEventData = serverSystem.createEventData("NormaConstructor:command")
-                commandEventData.data.command = "removeLastPosition"
-                commandEventData.data.playerID = playerID
-                serverSystem.broadcastEvent("NormaConstructor:command", commandEventData)
+                sendCommand("removeLastPosition", playerID)
                 break;
             }
             case "minecraft:iron_axe": {
                 //Remove the last blockType.
-                let commandEventData = serverSystem.createEventData("NormaConstructor:command")
-                commandEventData.data.command = "removeLastblockType"
-                commandEventData.data.playerID = playerID
-                serverSystem.broadcastEvent("NormaConstructor:command", commandEventData)
+                sendCommand("removeLastblockType", playerID)
                 break;
             }
             case "minecraft:golden_axe": {
                 //Choose the next generator.
-                let commandEventData = serverSystem.createEventData("NormaConstructor:command")
-                commandEventData.data.command = "chooseNextGenerator"
-                commandEventData.data.playerID = playerID
-                serverSystem.broadcastEvent("NormaConstructor:command", commandEventData)
+                sendCommand("chooseNextGenerator", playerID)
                 break;
             }
-            case "minecraft:diamond_axe":{
+            case "minecraft:diamond_axe": {
                 //Show currently saved data.
-                let commandEventData = serverSystem.createEventData("NormaConstructor:command")
-                commandEventData.data.command = "showSavedData"
-                commandEventData.data.playerID = playerID
-                serverSystem.broadcastEvent("NormaConstructor:command", commandEventData)
+                sendCommand("showSavedData", playerID)
                 break;
             }
             case "minecraft:stick": {
@@ -110,22 +64,17 @@ serverSystem.initialize = function () {
                 serverSystem.broadcastEvent("NormaConstructor:ExecutionRequest", executeRequestEventData)
                 break;
             }
-            case "minecraft:":{
+            case "minecraft:": {
 
             }
         }
     })
-    serverSystem.listenForEvent("minecraft:entity_use_item",(eventData)=>{
+    serverSystem.listenForEvent("minecraft:entity_use_item", (eventData) => {
         //TODO:Inspect why the log function here doesn't work...properly.(?)
         server.log("AAAAAAAAAAAAAAAA")
     })
-    serverSystem.listenForEvent("NormaConstructor:ExecutionResponse", (eventData) => {
-        //displayObject(eventData)
-        for (let block of eventData.data.blockArray) setBlock(block)
-    })
-    serverSystem.listenForEvent("NormaConstructor:setBlock",(eventData)=>{
-        setBlock(eventData.data.block)
-    })
+    serverSystem.listenForEvent("NormaConstructor:ExecutionResponse", (eventData) => { for (let block of eventData.data.blockArray) setBlock(block) })
+    serverSystem.listenForEvent("NormaConstructor:setBlock", (eventData) => setBlock(eventData.data.block))
 }
 
 serverSystem.update = function () {
@@ -142,6 +91,48 @@ function displayChat(message) {
     }
 }
 
+function getBlockType(eventData) {
+    let blockType = new BlockType(undefined, undefined)
+
+    let handContainer = serverSystem.getComponent(eventData.data.player, "minecraft:hand_container").data
+    let offHandItem = handContainer[1]
+    if (offHandItem.__identifier__ == "minecraft:shield") {//Since the player can't place air, holding a shield will represent so. 
+        blockType.blockIdentifier = "minecraft:air"
+        blockType.blockState = null
+    }
+    else {
+        let tickingArea = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.ticking_area
+        let block = serverSystem.getBlock(tickingArea, eventData.data.block_position)
+        blockType.blockIdentifier = block.__identifier__
+        blockType.blockState = serverSystem.getComponent(block, "minecraft:blockstate").data
+    }
+
+    let getblockTypeEventData = serverSystem.createEventData("NormaConstructor:getBlockType")
+    getblockTypeEventData.data.blockType = blockType
+    getblockTypeEventData.data.playerID = eventData.data.player.id
+    serverSystem.broadcastEvent("NormaConstructor:getBlockType", getblockTypeEventData)
+
+}
+function getPosition(eventData) {
+    let position = new Position(
+        new Coordinate(undefined, undefined, undefined),
+        undefined
+    )
+
+    position.coordinate = eventData.data.block_position
+    position.tickingArea = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.ticking_area
+
+    let getPositionEventData = serverSystem.createEventData("NormaConstructor:getPosition")
+    getPositionEventData.data.position = position
+    getPositionEventData.data.playerID = eventData.data.player.id
+    serverSystem.broadcastEvent("NormaConstructor:getPosition", getPositionEventData)
+}
+function sendCommand(command, playerID) {
+    let commandEventData = serverSystem.createEventData("NormaConstructor:command")
+    commandEventData.data.command = command
+    commandEventData.data.playerID = playerID
+    serverSystem.broadcastEvent("NormaConstructor:command", commandEventData)
+}
 function setBlock(block) {
 
     displayChat("§b We all agree, NZ is JULAO!")

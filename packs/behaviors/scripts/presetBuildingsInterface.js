6e7564fe9844c54df0b2a13fda37a77813ac7c22
DrZaofu <drzaofu@gmail.com>
2020-09-30 09:05:43 +0800

    修复打包插件中的bug

--- packs/behaviors/scripts/presetBuildingsInterface.js
+++ packs/behaviors/scripts/presetBuildingsInterface.js
@@ -1,4 +1,10 @@
 import * as subway_station from "./preset/subway_station.json"
+//TODO
+//制作像素字生成器
+//import * as mcfont from "./preset/font.json"
+//or
+//const mcfont = require("./preset/font")
+//
 //const subway_station=require("./preset/subway_station")
-const presetBuildings={subway_station}
+const presetBuildings={subway_station/*,mcfont*/}
 export {presetBuildings}

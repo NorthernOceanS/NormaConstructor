d664ac68177b132bb4c10e893878c07cdf1b1908
Shen Lang <shenlang@sjtu.edu.cn>
2021-01-21 20:19:19 +0800

    feat(*): initial a new branch for next generation
    Delete all behaviors scripts to create a new branch for
    the next generation of NormaConstructor.
    To know more about the next generation, see the documentation for that.
    
    BREAKING CHANGE: all scripts will be rewrite in the next generation
    
    If you are a generator developer, you should rewrite all your generators.
    Please read the documentation for the next generation.
    And you will see that it's not so diffifult to rewrite generators in new way.

--- packs/behaviors/scripts/presetBuildingsInterface.js
+++
@@ -1,4 +0,0 @@
-import * as subway_station from "./preset/subway_station.json"
-//const subway_station=require("./preset/subway_station")
-const presetBuildings={subway_station}
-export {presetBuildings}

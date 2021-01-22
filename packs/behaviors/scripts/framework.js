34f0a0d1905fce9210754580447caaf6b6730aef
Shen Lang <shenlang@sjtu.edu.cn>
2021-01-22 15:59:25 +0800

    feat(system): add some basic functions to class System
    Add some basic functions to class System.
    To see usage of these functions, please read the documentation.

--- packs/behaviors/scripts/framework.js
+++ packs/behaviors/scripts/framework.js
@@ -8,4 +8,30 @@ export const emptyPlatform = {
 }
 
 export class System {
+    constructor() {
+        this._platform = null;
+        this._generators = [];
+    }
+    /*
+    ** Following functions are used by platform.
+    ** If you are a generator developer,
+    ** please don't rely on these functions.
+    */
+    inject(platform) {
+        this._platform = platform;
+    }
+    /*
+    ** Following functions are used by users,
+    ** If you are a generator developer,
+    ** please don't rely on these functions.
+    */
+    getGenerators() {
+        return Array.from(this._generators);
+    }
+    /*
+    ** Following functions are register API of system.
+    */
+    registerGenerator(generator) {
+        this._generators.push(generator);
+    }
 }

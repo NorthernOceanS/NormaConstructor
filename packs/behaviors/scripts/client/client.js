e86a10a1c53a9f83b1928bc24b584010373a6572
Shen Lang <shenlang@sjtu.edu.cn>
2021-01-25 20:58:24 +0800

    refactor(logger): introduce the loggerFactory to replace logger
    By introducing loggerFactory, we can
    use loggerFactory(userSystem) to replace the logger.

--- packs/behaviors/scripts/client/client.js
+++ packs/behaviors/scripts/client/client.js
@@ -34,6 +34,26 @@ const platform = {
                 this.log(level, JSON.stringify(object, null, '    '))
             }
         }
+        function loggerFactory(user) {
+            return {
+                displayChat, displayObject,
+                log: function (level, message) {
+                    const colorMap = new Map([
+                        ["verbose", { num: 0, color: "§a" }],
+                        ["debug", { num: 1, color: "§6" }],
+                        ["info", { num: 2, color: "§b" }],
+                        ["warning", { num: 3, color: "§e" }],
+                        ["error", { num: 4, color: "§c" }],
+                        ["fatal", { num: 5, color: "§4" }]
+                    ])
+                    if (colorMap.get(level).num >= colorMap.get(user.session["__logLevel"]).num)
+                        this.displayChat(colorMap.get(level).color + "[" + level + "]" + message)
+                },
+                logObject: function (level, object) {
+                    this.log(level, JSON.stringify(object, null, '    '))
+                }
+            }
+        }
 
 
 

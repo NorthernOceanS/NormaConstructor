64df54e48fbf4eb9dd863c10c26b3631c2f43ffb
Shen Lang <shenlang@sjtu.edu.cn>
2021-01-26 22:23:57 +0800

    refactor(user): delete the playerOption
    Delete the playerOption since it is not used now.

--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -21,7 +21,6 @@ const platform = {
             serverSystem.broadcastEvent("minecraft:script_logger_config", scriptLoggerConfig);
 
             let blockStateToTileDataTable = new Map()
-            let playerOption = {};
 
             let compiler = {
                 raw: function (blockArray) {

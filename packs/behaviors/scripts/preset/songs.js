28964fb9fc87517dbffaa074193e593a10523b50
dzx-dzx <dzx_dzx@hotmail.com>
2021-02-28 16:43:31 +0800

    Add another mcstructure file that replace note block with command block, so the volume will be the same.

--- packs/behaviors/scripts/preset/songs.js
+++ packs/behaviors/scripts/preset/songs.js
@@ -1,7 +1,11 @@
 import * as bas0 from "./song/Bad_Apple_slower_0.json"
 import * as bas1 from "./song/Bad_Apple_slower_1.json"
+import * as croatian0 from"./song/Croatian Rhapsody-Maksim Mrvica_0.json"
+import * as croatian1 from"./song/Croatian Rhapsody-Maksim Mrvica_1.json"
 
 const songs=[]
 songs.push(bas0)
 songs.push(bas1)
+songs.push(croatian0)
+songs.push(croatian1)
 export {songs}

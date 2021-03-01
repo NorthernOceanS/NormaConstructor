4fcacf9ccdd2d13736e60ad43725b3c58ff4dad0
dzx-dzx <dzx_dzx@hotmail.com>
2021-03-01 10:42:44 +0800

    Be a good child, will you?

--- packs/behaviors/scripts/preset/songs.js
+++ packs/behaviors/scripts/preset/songs.js
@@ -1,11 +1,16 @@
 import * as bas0 from "./song/Bad_Apple_slower_0.json"
 import * as bas1 from "./song/Bad_Apple_slower_1.json"
-import * as croatian0 from"./song/Croatian Rhapsody-Maksim Mrvica_0.json"
-import * as croatian1 from"./song/Croatian Rhapsody-Maksim Mrvica_1.json"
+import * as croatian0 from "./song/Croatian Rhapsody-Maksim Mrvica_0.json"
+import * as croatian1 from "./song/Croatian Rhapsody-Maksim Mrvica_1.json"
+import * as ha0 from "./song/Undertale_OST__Heartache_-_Piano_0.json"
+import * as ha1 from "./song/Undertale_OST__Heartache_-_Piano_1.json"
 
-const songs=[]
+
+const songs = []
 songs.push(bas0)
 songs.push(bas1)
 songs.push(croatian0)
 songs.push(croatian1)
-export {songs}
+songs.push(ha0)
+songs.push(ha1)
+export { songs }

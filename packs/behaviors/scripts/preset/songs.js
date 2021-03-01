5d07e9fc855d5eefd752fcd13f43e1e14d26f121
dzx-dzx <dzx_dzx@hotmail.com>
2021-03-01 13:20:45 +0800

    Fix a type in the command from the structure.

--- packs/behaviors/scripts/preset/songs.js
+++ packs/behaviors/scripts/preset/songs.js
@@ -4,13 +4,15 @@ import * as croatian0 from "./song/Croatian Rhapsody-Maksim Mrvica_0.json"
 import * as croatian1 from "./song/Croatian Rhapsody-Maksim Mrvica_1.json"
 import * as ha0 from "./song/Undertale_OST__Heartache_-_Piano_0.json"
 import * as ha1 from "./song/Undertale_OST__Heartache_-_Piano_1.json"
-
+import * as re0 from"./song/Etude_in_C_Minor_Revolutionary_0.json"
+import * as re1 from"./song/Etude_in_C_Minor_Revolutionary_1.json"
+import * as sa0 from"./song/Samurai with a Mission_0.json"
+import * as sa1 from"./song/Samurai with a Mission_1.json"
 
 const songs = []
-songs.push(bas0)
-songs.push(bas1)
-songs.push(croatian0)
-songs.push(croatian1)
-songs.push(ha0)
-songs.push(ha1)
+songs.push(bas0,bas1)
+songs.push(croatian0,croatian1)
+songs.push(ha0,ha1)
+songs.push(re0,re1)
+songs.push(sa0,sa1)
 export { songs }

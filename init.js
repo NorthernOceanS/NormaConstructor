82457d8a3003729bf5d9b32eeeadcd11e050a6c9
Shen Lang <shenlang@sjtu.edu.cn>
2021-05-13 22:43:34 +0800

    feat(test): emulate behavior of scripting correctly

--- init.js
+++ init.js
@@ -12,12 +12,19 @@ function TimeoutPromise(ms) {
 (async function(){
 	serverSystem.initialize();
 	clientSystem.initialize();
+	await TimeoutPromise(0)
 	channel.port2.postMessage({
 		eventIdentifier: "minecraft:client_entered_world",
 		eventData: {data: {player:{__unique_id__: 1}}}
 	});
-	//serverSystem.update();
-	//clientSystem.update();
-	clientSystem.shutdown();
-	serverSystem.shutdown();
+	await TimeoutPromise(0);
+	for(let i = 0; i < 20; i++) {
+		let wait = TimeoutPromise(1000/20);
+		serverSystem.update && serverSystem.update();
+		clientSystem.update && clientSystem.update();
+		await wait;
+	}
+	await TimeoutPromise(0);
+	clientSystem.shutdown && clientSystem.shutdown();
+	serverSystem.shutdown && serverSystem.shutdown();
 })();

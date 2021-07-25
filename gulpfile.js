5e6b776b9d91a1fa2aaf7962a6388add775e8e2d
Shen Lang <shenlang@sjtu.edu.cn>
2021-07-25 23:06:31 +0800

    feat(toolchain): automatically generate index.js of plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v1");
 const BrowserifySupport = require("minecraft-addon-toolchain-browserify");
+const AutoFileGenerationSupport = require("./file-generation.js");
 
 const builder = new MinecraftAddonBuilder("NormaConstructor");
 
 //!!!!!!!!!!!!!!!!!!!!
 const browserifySupport = new BrowserifySupport();
+const autoFileGenerationSupport = new AutoFileGenerationSupport();
 browserifySupport.bundleSources.push("scripts/**/*.*");
+autoFileGenerationSupport.bundleDir = browserifySupport.intermediateDir;
 
+builder.addPlugin(autoFileGenerationSupport);
 builder.addPlugin(browserifySupport);
 
 module.exports = builder.configureEverythingForMe();

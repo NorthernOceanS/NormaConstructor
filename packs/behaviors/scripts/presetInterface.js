396cc5029ff7260654d8e851071ddc218c287cf1
DrZaofu <59088107+DrZaofu@users.noreply.github.com>
2021-08-14 07:41:22 +0800

    Update presetInterface.js

--- packs/behaviors/scripts/presetInterface.js
+++ packs/behaviors/scripts/presetInterface.js
@@ -1,6 +1,6 @@
 import * as subway_station from "./preset/subway_station.json"
 import { songs } from "./preset/songs.js"
-import { songs } from "./preset/font.js"
+import { mcfont } from "./preset/font.js"
 //const subway_station=require("./preset/subway_station")
 const preset = { subway_station, songs, mcfont }
 export { preset }
